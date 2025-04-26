1363a2796ae239234355d4d59562f1f17a313e8b
Siddhant Kumar <siddhantk.0704@gmail.com>
2025-04-26 16:26:40 -0400

    Updated code

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def display_page(pathname, dashboard_data):
 # Run the app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
